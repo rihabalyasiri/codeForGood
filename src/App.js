04f28378c2ff9df85749e03cc6e8a2854eee2a7b
rihab <rehabalyasiri@yahoo.com>
2019-12-03 21:30:44 +0100

    testing localstorage

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    localStorage.clear()
+    //localStorage.clear()
     if (localStorage.getItem('dataFromUser') === null) {
       localStorage.setItem('dataFromUser', JSON.stringify(Datas))
     }
